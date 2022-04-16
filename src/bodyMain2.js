dae3fb3df1140fc4f8f1ba5a61be4f29b08e4dfa
Abhi7012 <72123024+Abhi7012@users.noreply.github.com>
2022-04-17 02:03:12 +0530

    changed positioning

--- src/bodyMain2.js
+++ src/bodyMain2.js
@@ -4,7 +4,7 @@ function Contentmid() {
     return (
         <div className="contentmid"><div className="container imgbox" >
             <img src={process.env.PUBLIC_URL+"/webdev.svg"} id="mainimg" alt="not loaded" />
-            <h3>I am a web developer ,Who would love to work for you </h3>
+            <h3 style={{textAlign:'center',display:'block'}}>I am a web developer <br></br>Who would love to work for you </h3>
             
         </div>
         </div>
