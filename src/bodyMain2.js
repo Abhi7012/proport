775bb256e948ca9705a9395b87469c91a6af678c
Abhi7012 <72123024+Abhi7012@users.noreply.github.com>
2022-04-18 00:51:56 +0530

    changed some css

--- src/bodyMain2.js
+++ src/bodyMain2.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Contentmid() {
     return (
-        <div className="contentmid"><div className="container imgbox" >
+        <div className="contentmid">
+        <div className="container imgbox" >
             <img src={process.env.PUBLIC_URL+"/webdev.svg"} id="mainimg" alt="not loaded" />
-            <h3 style={{textAlign:'center',display:'block'}}>I am a web developer <br></br>Who would love to work for you </h3>
             
         </div>
         </div>
--- src/projects.js
+++ src/projects.js
@@ -5,6 +5,8 @@ import Slider from './slider';
 function Projects() {
     return (
         <div className="Projects" id="projectlist" data-scroll-index="1">
+                <h3 style={{textAlign:'center',display:'block',paddingBottom:'2rem'}}>I am a web developer <br></br>Who would love to work for you </h3>
+
             <h4>
                 Here are some of my projects
             </h4>
